d26d49e6b40ccb583cedf25c54fb7587aa8b3b8f
ayanmukhopadhyay <ayan.mukhopadhyay@vanderbilt.edu>
2015-10-11 00:35:39 -0500

    fabric test

--- testFabric.py
+++ testFabric.py
@@ -1,3 +1,13 @@
-__author__ = 'ayanmukhopadhyay'
+from fabric.api import env, execute, task
+import cuisine
 
+env.hosts = "10.10.1.76"
+env.user = "mukhopa"
+env.password = "ayan12345"
+env.key_filename = "ayan_horizon.pem"
 
+@task
+def update():
+    sudo('apt-get update')
+
+execute(update)
