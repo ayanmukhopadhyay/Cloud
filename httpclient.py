869b0d2cdd886bab0e54f390161ffc7744a4aaef
ayanmukhopadhyay <ayan.mukhopadhyay@vanderbilt.edu>
2015-10-20 20:13:23 +0000

    client print removed

--- httpclient.py
+++ httpclient.py
@@ -8,6 +8,7 @@
 import sys
 import httplib
 from datetime import datetime
+import random
 
 def main ():
     print "Instantiating a connection obj"
@@ -16,11 +17,13 @@ def main ():
     except:
         print "Exception thrown: ", sys.exc_info()[0]
         raise
-    for counter in range(1000):
+    for counter in range(100):
         print "sending a GET request to our http server"
         try:
+	    numberCheck = str(random.randint(1,100))
             timePre = datetime.now()
-            conn.request("GET", "?53-isNumberPrime")
+	    
+            conn.request("GET", "?"+numberCheck+"-isNumberPrime")
             # conn.request("GET", "/testurl", str(53))
         except:
             print "Exception thrown: ", sys.exc_info()[0]
@@ -29,19 +32,19 @@ def main ():
         print "retrieving a response from http server"
         try:
             resp = conn.getresponse ()
-            timePost = datetime.now()
-            latency = (timePost - timePre).total_seconds()
-            print latency
+            #timePost = datetime.now()
+            #latency = (timePost - timePre).total_seconds()
+            #print latency
         except:
             print "Exception thrown: ", sys.exc_info()[0]
             raise
-        try:
-            print "sending latency report back to server"
-            conn.request("GET","?"+str(latency)+"-latencyReport")
-            resp = conn.getresponse ()
-        except:
-            print "Exception thrown: ", sys.exc_info()[0]
-            raise
+        #try:
+            #print "sending latency report back to server"
+            #conn.request("GET","?"+str(latency)+"-latencyReport")
+            #resp = conn.getresponse ()
+        #except:
+            #print "Exception thrown: ", sys.exc_info()[0]
+            #raise
 
         print "printing response headers"
         try:
