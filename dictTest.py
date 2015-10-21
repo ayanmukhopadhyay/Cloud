c6b7a61960a257c7247b7b1d44c939dc47e7f3cd
ayanmukhopadhyay <ayan.mukhopadhyay@vanderbilt.edu>
2015-10-20 19:17:56 -0500

    removed sys exit

--- dictTest.py
+++ dictTest.py
@@ -1,8 +1,12 @@
 __author__ = 'ayanmukhopadhyay'
 from itertools import cycle
+import datetime
+import numpy as np
 
-testDictionary = {"ayan":[1,2,3]}
-testDictionary["ayan"].append(4)
+testDictionary = {"ayan":[[1,2,3],[4]]}
+testDictionary["ayan"][0].append(4)
+testDictionary["ayan"][1].append(4)
+
 print testDictionary
 
 list = [1,2,3]
@@ -15,3 +19,9 @@ print next(a)
 print next(a)
 print next(a)
 print next(a)
+print a
+
+latency = np.empty((len()))
+latency = [[1,2,3,4,5,6],[datetime.datetime(2009,1,1),datetime.datetime(2009,1,1),datetime.datetime(2009,1,1),datetime.datetime(2009,1,1),datetime.datetime(2009,1,1),datetime.datetime(2009,1,1)]]
+latency = np.array(latency)
+latencies = latency[np.argsort(latency[:,1])]
